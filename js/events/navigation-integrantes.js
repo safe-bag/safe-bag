1326487dfc86e121833918e497acc92fcd1d0041
iagoliziero <iagolizierox14@gmail.com>
2025-06-02 22:22:42 -0300

    feat: inserted links for navigations

---
+++ js/events/navigation-integrantes.js
@@ -0,0 +1,52 @@
+//Navigation LinkedIn and GitHub
+
+//Navigation linkedin (IAGO)
+const imgLinkedinIago = document.querySelector('[data-icon-linkedin-iago]');
+const navigationLinkedinIago = document.querySelector('[data-navigation-linkedin-iago]');
+
+imgLinkedinIago.addEventListener('click', () => {
+    navigationLinkedinIago.click();
+})
+
+//Navigation GitHub (IAGO)
+
+const imgGithubIago = document.querySelector('[data-icon-github-iago]');
+const navigationGithubIago = document.querySelector('[data-navigation-github-iago]');
+
+imgGithubIago.addEventListener('click', () => {
+    navigationGithubIago.click();
+})
+
+//Navigation linkedin (VITOR)
+const imgLinkedinVitor = document.querySelector('[data-icon-linkedin-vitor]');
+const navigationLinkedinVitor = document.querySelector('[data-navigation-linkedin-vitor]');
+
+imgLinkedinVitor.addEventListener('click', () => {
+    navigationLinkedinVitor.click();
+})
+
+//Navigation GitHub (VITOR)
+
+const imgGithubVitor = document.querySelector('[data-icon-github-vitor]');
+const navigationGithubVitor = document.querySelector('[data-navigation-github-vitor]');
+
+imgGithubVitor.addEventListener('click', () => {
+    navigationGithubVitor.click();
+})
+
+//Navigation linkedin (VITOR)
+const imgLinkedinEnrico = document.querySelector('[data-icon-linkedin-enrico]');
+const navigationLinkedinEnrico = document.querySelector('[data-navigation-linkedin-enrico]');
+
+imgLinkedinEnrico.addEventListener('click', () => {
+    navigationLinkedinEnrico.click();
+})
+
+//Navigation GitHub (VITOR)
+
+const imgGithubEnrico = document.querySelector('[data-icon-github-enrico]');
+const navigationGithubEnrico = document.querySelector('[data-navigation-github-enrico]');
+
+imgGithubEnrico.addEventListener('click', () => {
+    navigationGithubEnrico.click();
+})
